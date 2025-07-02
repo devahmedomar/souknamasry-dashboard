a17f182b4f5d76e1ef61d88a6fdb87592a025924
shahd12121 <zazaza909090.zaza9z0a@gmail.com>
2025-07-02 18:55:36 +0300

    create SearchSplitButton component with cascaded filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SearchSplitButtonComponent } from "./shared/components/search-split-button/search-split-button.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SearchSplitButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
