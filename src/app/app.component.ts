a4081d632f0e972030e63569a788e7bdf2f4b076
shahd12121 <zazaza909090.zaza9z0a@gmail.com>
2025-07-13 20:32:21 +0300

    resolve conflicts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { DateRangeComponent } from "./shared/components/date-range/date-range.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ DateRangeComponent],
+  imports: [ ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { DateRangeComponent } from '../../shared/components/date-range/date-range.component';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [DateRangeComponent],
+  imports: [],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
