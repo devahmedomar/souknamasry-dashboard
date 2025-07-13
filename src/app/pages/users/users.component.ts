a98024bb116f72b1fddfb3180fa609c4a9e08c03
Ahmed-Muhammad22 <ahmed.khater22698@gmail.com>
2025-07-13 18:52:41 +0300

    put date range in user component

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { DateRangeComponent } from '../../shared/components/date-range/date-range.component';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [DateRangeComponent],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
